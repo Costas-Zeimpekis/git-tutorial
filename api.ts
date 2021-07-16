dd246125ec0bd45c03f28f576bcc80f023fff549
ElisavetAmpatzidou <elisabet.ampatzidou@gmail.com>
2021-07-17 01:13:54 +0300

    exercise 8, create api.ts

---
+++ api.ts
@@ -0,0 +1,22 @@
+interface Post{
+    postId: number;
+    id: number;
+    title: string;
+    body: string;
+}
+
+interface Comment{
+    postId: number;
+    id: number;
+    name: string;
+    email: string;
+    body: string;
+}
+
+export function posts(){
+    return fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json());
+}
+
+export function comments(){
+    return fetch('https://jsonplaceholder.typicode.com/comments').then(res => res.json());
+}
