00151f7db977ff3b3a86aa06cad937bb068c5bf4
ElisavetAmpatzidou <elisabet.ampatzidou@gmail.com>
2021-07-18 18:43:20 +0300

    added files for exercise 8

--- helper.ts
+++ helper.ts
@@ -1,13 +1,13 @@
-import {posts as getPosts, comments as getComments } from './api';
+import { Calculator } from "./interfaces";
 
-class Animal{
+export class Animal{
     origins: string;
     constructor(origins : string){
         this.origins = origins;
     }
 }
 
-class Dog extends Animal{
+export default class Dog extends Animal{
     breed: string;
     hairColor: string;
     constructor(origins : string, breed: string, hairColor: string){
@@ -15,4 +15,21 @@ class Dog extends Animal{
         this.breed = breed;
         this.hairColor = hairColor;
     }
+}
+
+export function calculator( param: Calculator) {
+    let result;
+    if (param.type === "addition"){
+        result = param.num1 + param.num2;
+    }
+    else if (param.type === "substraction"){
+        result = param.num1 - param.num2;
+    }
+    else if (param.type === "multipication"){
+        result = param.num1 * param.num2;
+    }
+    else if (param.type === "division"){
+        result = param.num1 / param.num2;
+    }
+    return result;
 }
