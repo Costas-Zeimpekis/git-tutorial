985a5936cf472cade09e9edc12cff15b49fc82a6
George Giagkos <giwrgos.giagk91@gmail.com>
2021-07-18 22:05:38 +0300

    Classes exercises

---
+++ index.ts
@@ -0,0 +1,17 @@
+interface UnknownProps {
+    [key:string]: string | number;
+}
+
+
+
+
+const student: UnknownProps = {
+    fistName: "Costas",
+    lastName: "Zeimpekis"
+  };
+  
+  const grades: UnknownProps = {
+    math: 20,
+    chemistry: 20,
+    history: 30
+  };
